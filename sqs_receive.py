11fa8b2c036a4aa3dd15b5268cddf60da0fbcb55
Larissa Matos <larissamatos07.lm@gmail.com>
2024-04-12 22:02:07 -0300

    script de recebimento de mensagem na fila sqs

---
+++ sqs_receive.py
@@ -0,0 +1,29 @@
+import boto3
+
+
+sqs = boto3.client('sqs', region_name='us-east-1')
+queue_url = 'https://sqs.us-east-1.amazonaws.com/117793179715/myQueue'
+
+# Receive message from SQS queue
+response = sqs.receive_message(
+    QueueUrl=queue_url,
+    AttributeNames=[
+        'SentTimestamp'
+    ],
+    MaxNumberOfMessages=1,
+    MessageAttributeNames=[
+        'All'
+    ],
+    VisibilityTimeout=0,
+    WaitTimeSeconds=0
+)
+
+message = response['Messages'][0] 
+receipt_handle = message['ReceiptHandle']
+
+# Delete received message from queue
+sqs.delete_message(
+    QueueUrl=queue_url,
+    ReceiptHandle=receipt_handle
+)
+print('Received and deleted message: %s' % message)
